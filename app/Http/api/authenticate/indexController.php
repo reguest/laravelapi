9c3f2287ece01c414d27152b3e3f9f01d2cc1a92
orhan <67384523+reguest@users.noreply.github.com>
2022-02-10 17:25:29 +0300

    laravel1

---
+++ app/Http/api/authenticate/indexController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\api\authenticate;
+
+use App\Http\Controllers\api\BaseController;
+
+class indexController extends BaseController{
+    
+    public function
+}
